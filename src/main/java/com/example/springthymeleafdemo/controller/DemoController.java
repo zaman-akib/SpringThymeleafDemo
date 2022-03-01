a84b15216a3ea2ad6f5828dc738504a6788f9d7b
Akib Uz Zaman <akib.zaman.04@gmail.com>
2022-03-01 15:10:54 +0600

    initial commit

---
+++ src/main/java/com/example/springthymeleafdemo/controller/DemoController.java
@@ -0,0 +1,73 @@
+package com.example.springthymeleafdemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DemoController {
+
+    @GetMapping(value ="/home")
+    public String home() {
+        return "home";
+    }
+
+    @GetMapping(value = "/about")
+    public String about() {
+        return "about";
+    }
+
+    @GetMapping(value = {"/registration-ongoing", "/"})
+    public String registrationOngoing() {
+        return "registration-ongoing";
+    }
+
+    @GetMapping(value = "/student-sign-in")
+    public String studentSignIn() {
+        return "student-sign-in";
+    }
+
+    @GetMapping(value = "/student-registration-ahu")
+    public String studentRegistrationAHU() {
+        return "student-registration-ahu";
+    }
+
+    @GetMapping(value = "/student-registration-non-ahu")
+    public String studentRegistrationNonAHU() {
+        return "student-registration-non-ahu";
+    }
+
+    @GetMapping(value = "/invalid-info")
+    public String invalidData() {
+        return "invalid-info";
+    }
+
+    @GetMapping(value = "/existing-account")
+    public String existingAccount() {
+        return "existing-account";
+    }
+
+    @GetMapping(value = "/account-verification")
+    public String accountVerification() {
+        return "account-verification";
+    }
+
+    @GetMapping(value = "/set-password")
+    public String setPassword() {
+        return "set-password";
+    }
+
+    @GetMapping(value = "/registration-success")
+    public String registrationSuccess() {
+        return "registration-success";
+    }
+
+    @GetMapping(value = "/student-id/student-dashboard")
+    public String studentDashboard() {
+        return "student-dashboard";
+    }
+
+    @GetMapping(value = "/student-id/registration-progress")
+    public String registrationProgress() {
+        return "registration-progress";
+    }
+}
